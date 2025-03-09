d67d9a73f9fbaf5c2cc425f03bec1888f6e61d79
mohanedii <hanob0269@gmail.com>
2025-03-09 18:22:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '', //Put Session-id Here
+SESSION_ID: process.env.SESSION_ID || 'SADIYA-MD=FBoGXB4b#nwoq8wAsiG_THozgRlbqjwFyaLhj1Yy9Mob9kLI9OIs', //Put Session-id Here
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || '', //Put Your Gitub Username
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '', //Put Your Github Auth Token
 DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || 'SADIYA-MD-DATABASE' //Bot Database Repo Name
